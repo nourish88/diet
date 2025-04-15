b2ef5c3a9f6c7d9725014fa099d568958f9c199a
nuriaktas <aktasnuri88@gmail.com>
2025-04-15 16:13:36 +0300

    adsad

--- app/api/besins/route.ts
+++ app/api/besins/route.ts
@@ -4,13 +4,18 @@ import prisma from "@/lib/prisma";
 export async function GET(request: NextRequest) {
   try {
     const besins = await prisma.besin.findMany({
-      orderBy: {
-        description: "asc",
+      orderBy: [
+        {
+          priority: "desc",
+        },
+        {
+          name: "asc",
+        },
+      ],
+      include: {
+        besinGroup: true,
       },
     });
-
-    console.log("MultiBesinSelector received selectedBesins:", selectedBesins);
-    console.log("safeSelectedBesins after processing:", safeSelectedBesins);
 
     return NextResponse.json({ besins });
   } catch (error) {
