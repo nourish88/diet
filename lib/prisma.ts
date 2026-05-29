c528faafcbd82e2442384f260e63f227619a0ce6
nuriaktas <aktasnuri88@gmail.com>
2026-05-30 01:21:11 +0300

    chore(prisma): reduce log noise by environment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+const logLevels: Prisma.LogLevel[] =
+  process.env.NODE_ENV === 'production' ? ['error'] : ['warn', 'error']
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query'],
+    log: logLevels,
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
